9fffeff8468879ba486c472628c4e5cbcf4f847d
Ashin.p.s <ashinps3559@gmail.com>
2023-08-11 16:21:10 +0530

    Initial commit

--- lib/Screens/Home/homepage.dart
+++ lib/Screens/Home/homepage.dart
@@ -1,5 +1,5 @@
-import 'package:trip_ease_project/Screens/settings/setting.dart';
 import 'package:flutter/material.dart';
+import 'package:trip_ease_project/Screens/settings/setting.dart';
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
 
@@ -8,17 +8,80 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  int _currentIndex = 0;
+
+  final tabs = [
+    Home(),
+    Maps(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),),
-
-      drawer: const Drawer(child: Settings()),
-
-      body: const Center(
-        child: Text('Welcome!'),
+        title: const Text('Trip Ease'),),
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text("Settings"),
+              onTap:  () async {
+              await Navigator.push(context,
+                MaterialPageRoute(builder: (context) => Settings()),);}
+            ),
+          ],
+        ),
       ),
+
+      body: tabs[_currentIndex],
+
+
+      bottomNavigationBar:BottomNavigationBar(
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        fixedColor: Colors.white,
+        backgroundColor: Colors.red,
+        selectedIconTheme: const IconThemeData(size: 30,),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+           ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map_rounded),
+            label: 'Maps',
+            ),
+        ],
+        onTap: (index){
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      )
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Text('Home is here')),
+    );
+  }
+}
+
+class Maps extends StatelessWidget {
+  const Maps ({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Text('Maps is here')),
     );
   }
 }
--- lib/Screens/register/register_page.dart
+++ lib/Screens/register/register_page.dart
@@ -26,146 +26,151 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
+      body: ListView(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
 
-            //Title
-            const Text("Register", style: TextStyle(fontSize: 30),),
-
-
-            //Registration Form
-            Form(
-              key: _formKey,
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  children: [
+                //Title
+                const Text("Register", style: TextStyle(fontSize: 30),),
 
 
-                    //username field
-                    TextFormField(
-                      controller: usernameController,
-                      decoration: const InputDecoration(
-                        labelText: "Name",
-                        border: OutlineInputBorder(),
-                      ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty|| !RegExp(r'^[a-zA-Z0-9_-]{3,20}$').hasMatch(value) ) {
-                            return 'Please enter your name';
-                          }
-                          return null;
-                        }
-                    ),
+                //Registration Form
+                Form(
+                  key: _formKey,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      children: [
 
 
-                    const SizedBox(height: 20),
-
-
-                    //email address field
-                    TextFormField(
-                      controller: emailController,
-                      decoration: const InputDecoration(
-                        hintText: 'example@gmail.com',
-                        labelText: "Email",
-                        border: OutlineInputBorder(),
-                      ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty||  !RegExp(r'^[\w-]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
-                            return 'Please enter your email';
-                          }
-                          return null;
-                        }
-                    ),
-
-
-                    const SizedBox(height: 20),
-
-
-                    //password field
-                    TextFormField(
-                      obscureText: !passEnable,
-                      controller: passController,
-                      decoration: InputDecoration(
-                        hintText: '(8+chars,1lowercase,1uppercase,1digit,1symbol)',
-                        labelText: "Password",
-                        border: const OutlineInputBorder(),
-                        suffixIcon: IconButton(
-                          onPressed: () {
-                            setState(() {
-                              passEnable = !passEnable;
-                            });
-                          },
-                          icon: Icon(passEnable ? Icons.visibility:Icons.visibility_off))),
-                        validator: (value) {
-                          if (value == null || value.isEmpty|| !RegExp(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$').hasMatch(value)) {
-                            return 'Please enter your password';
-                          }
-                          return null;
-                        },
-                    ),
-
-
-                    const SizedBox(height: 20),
-
-
-                    //Confirm password field
-                    TextFormField(
-                      obscureText: !confirmPassEnable,
-                      controller: confirmPassController,
-                      decoration: InputDecoration(
-                        labelText: "Confirm Password",
-                        border: const OutlineInputBorder(),
-                          suffixIcon: IconButton(
-                          onPressed: () {
-                            setState(() {
-                              confirmPassEnable = !confirmPassEnable;
-                            });
-                          },
-                          icon: Icon(confirmPassEnable ? Icons.visibility:Icons.visibility_off))
-                      ),
-                        validator: (value) {
-                          if (value == null || value.isEmpty|| value != passController.text) {
-                            return 'Please confirm your password';
-                          }
-                          return null;
-                        },
-                    ),
-
-                    const SizedBox(height: 20),
-
-                    //Register button
-                    SizedBox(
-                      height:40, width: 200,
-                      child: ElevatedButton(
-                        onPressed:(){
-                          setState(() {
-                            if(_formKey.currentState!.validate()){
-                              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const Homepage(),));
+                        //username field
+                        TextFormField(
+                          controller: usernameController,
+                          decoration: const InputDecoration(
+                            labelText: "Name",
+                            border: OutlineInputBorder(),
+                          ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty|| !RegExp(r'^[a-zA-Z0-9_-]{3,20}$').hasMatch(value) ) {
+                                return 'Please enter your name';
+                              }
+                              return null;
                             }
-                          });
-                        },
-                        child: const Text('Register',style: TextStyle(
-                          fontSize: 15
-                        ),),
-                      ),
-                    ),
+                        ),
 
-                    //Goto Login page
-                    const SizedBox(height: 20),
-                    Align(alignment:Alignment.center,child:Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children:[const Text('Already have an account?'),
-                        TextButton(onPressed: (){
-                          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const Loginpage(),));
-                        }, child: (const Text('Login',style: TextStyle(fontSize: 15),)))])),
-                  ],
+
+                        const SizedBox(height: 20),
+
+
+                        //email address field
+                        TextFormField(
+                          controller: emailController,
+                          decoration: const InputDecoration(
+                            hintText: 'example@gmail.com',
+                            labelText: "Email",
+                            border: OutlineInputBorder(),
+                          ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty||  !RegExp(r'^[\w-]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
+                                return 'Please enter your email';
+                              }
+                              return null;
+                            }
+                        ),
+
+
+                        const SizedBox(height: 20),
+
+
+                        //password field
+                        TextFormField(
+                          obscureText: !passEnable,
+                          controller: passController,
+                          decoration: InputDecoration(
+                            hintText: '(8+chars,1lowercase,1uppercase,1digit,1symbol)',
+                            labelText: "Password",
+                            hintStyle: TextStyle(fontSize: 14),
+                            border: const OutlineInputBorder(),
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  passEnable = !passEnable;
+                                });
+                              },
+                              icon: Icon(passEnable ? Icons.visibility:Icons.visibility_off))),
+                            validator: (value) {
+                              if (value == null || value.isEmpty|| !RegExp(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,16}$').hasMatch(value)) {
+                                return 'Please enter your password';
+                              }
+                              return null;
+                            },
+                        ),
+
+
+                        const SizedBox(height: 20),
+
+
+                        //Confirm password field
+                        TextFormField(
+                          obscureText: !confirmPassEnable,
+                          controller: confirmPassController,
+                          decoration: InputDecoration(
+                            labelText: "Confirm Password",
+                            border: const OutlineInputBorder(),
+                              suffixIcon: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  confirmPassEnable = !confirmPassEnable;
+                                });
+                              },
+                              icon: Icon(confirmPassEnable ? Icons.visibility:Icons.visibility_off))
+                          ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty|| value != passController.text) {
+                                return 'Please confirm your password';
+                              }
+                              return null;
+                            },
+                        ),
+
+                        const SizedBox(height: 20),
+
+                        //Register button
+                        SizedBox(
+                          height:40, width: 200,
+                          child: ElevatedButton(
+                            onPressed:(){
+                              setState(() {
+                                if(_formKey.currentState!.validate()){
+                                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const Homepage(),));
+                                }
+                              });
+                            },
+                            child: const Text('Register',style: TextStyle(
+                              fontSize: 15
+                            ),),
+                          ),
+                        ),
+
+                        //Goto Login page
+                        const SizedBox(height: 20),
+                        Align(alignment:Alignment.center,child:Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children:[const Text('Already have an account?'),
+                            TextButton(onPressed: (){
+                              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const Loginpage(),));
+                            }, child: (const Text('Login',style: TextStyle(fontSize: 15),)))])),
+                      ],
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
